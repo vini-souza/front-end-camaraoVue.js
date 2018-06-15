3b6a931146b55ca6eb138761f77c7d23023521c1
Vinicius de Souza <vinicius.ss195@gmail.com>
2018-06-14 21:18:29 -0300

    First Test

--- PrjCamarao-Front/src/App.vue
+++ PrjCamarao-Front/src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  
+  <div>
+    <CcHello></CcHello>
   </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import CcHello from './components/HelloWorld'
 
 export default {
-  name: 'App',
+  name: 'ShrimpFarmComponent',
   components: {
-    HelloWorld
+    CcHello
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ PrjCamarao-Front/src/components/Intro.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Test2</h1>
+        <p>
+        Curabitur laoreet nunc sit amet ipsum molestie, in volutpat nisi lobortis. 
+        Cras rutrum sapien et quam efficitur tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et 
+        ultrices posuere cubilia Curae; Donec justo augue, semper vitae egestas at, convallis ut nisi. Vivamus dictum elit quis risus 
+        hendrerit posuere. Suspendisse eleifend ante a erat hendrerit, eu pellentesque sapien viverra. Mauris at cursus dolor. Nunc f
+        eugiat dapibus facilisis. Donec egestas lobortis urna, vel malesuada ante. In hac habitasse platea dictumst. Morbi faucibus nulla vitae l
+        obortis tincidunt. Phasellus ante tellus, auctor sit amet lorem quis, sodales elementum lacus. Phasellus et quam ut nibh dapibus blandit vitae 
+        sit amet nisl. Donec venenatis dolor vel quam rutrum, at gravida felis consequat.
+        </p>
+  </div>
+
+</template>
+
+<script>
+export default {
+ }
+
+</script>
+
+
+<style scoped>
+  p{
+      color: blue;
+  }
+    
+
+</style>
